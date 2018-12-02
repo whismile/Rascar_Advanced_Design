42a0efc9a6395fbda1d1907049c12642ab05f928
Angheng <bungae1112@gmail.com>
2018-12-02 11:28:06 +0900

    Add Yellow Condition (Must be TroubleShoting)

--- colour_detector.py
+++ colour_detector.py
@@ -6,39 +6,46 @@ class Colour_Detector(object):
     def __init__(self):
         self.colour_detector = TCS34725_RGB.TCS34725()
 
-        self.red_range = [[100, 500], [0, 100], [0, 100]]
-        self.green_range = [[0, 100], [100, 500], [0, 100]]
-        self.blue_range = [[0, 100], [0, 100], [100, 500]]
+        self.red_range = [[100, 255], [0, 155], [0, 155]]
+        self.green_range = [[0, 155], [100, 255], [0, 155]]
+        self.blue_range = [[0, 155], [0, 155], [100, 255]]
+        self.yellow_range = [[100, 255], [100, 255], [0, 200]]
 
     def is_red(self):
-        return self.get_red() > self.get_green() + 200 and self.get_red() > self.get_blue() + 200 \
+        return self.get_red() > self.get_green() + 50 and self.get_red() > self.get_blue() + 50 \
                and self.red_range[0][0] <= self.get_red() <= self.red_range[0][1] \
                and self.red_range[1][0] <= self.get_green() <= self.red_range[1][1] \
                and self.red_range[2][0] <= self.get_blue() <= self.red_range[2][1]
 
+    def is_yellow(self):
+        return self.get_red() > self.get_blue() + 50 and self.get_green() > self.get_blue() + 50 \
+               and self.yellow_range[0][0] <= self.get_red() <= self.yellow_range[0][1] \
+               and self.yellow_range[1][0] <= self.get_green() <= self.yellow_range[1][1] \
+               and self.yellow_range[2][0] <= self.get_blue() <= self.yellow_range[2][1]
+
     def is_green(self):
-        return self.get_green() > self.get_blue() + 200 and self.get_green() > self.get_blue() + 200 \
+        return self.get_green() > self.get_blue() + 50 and self.get_green() > self.get_blue() + 50 \
                and self.green_range[0][0] <= self.get_red() <= self.green_range[0][1] \
                and self.green_range[1][0] <= self.get_green() <= self.green_range[1][1] \
                and self.green_range[2][0] <= self.get_blue() <= self.green_range[2][1]
 
     def is_blue(self):
-        return self.get_blue() > self.get_red() + 200 and self.get_blue() > self.get_green() + 200 \
+        return self.get_blue() > self.get_red() + 50 and self.get_blue() > self.get_green() + 50 \
                and self.blue_range[0][0] <= self.get_red() <= self.blue_range[0][1] \
                and self.blue_range[1][0] <= self.get_green() <= self.blue_range[1][1] \
                and self.blue_range[2][0] <= self.get_blue() <= self.blue_range[2][1]
 
     def get_red(self):
-        return self.colour_detector.get_raw_data()[0]
+        return int((255 / 1024) * self.colour_detector.get_raw_data()[0])
 
     def get_green(self):
-        return self.colour_detector.get_raw_data()[1]
+        return int((255 / 1024) * self.colour_detector.get_raw_data()[1])
 
     def get_blue(self):
-        return self.colour_detector.get_raw_data()[2]
+        return int((255 / 1024) * self.colour_detector.get_raw_data()[2])
 
     def get_clear(self):
-        return self.colour_detector.get_raw_data()[3]
+        return int((255 / 1024) * self.colour_detector.get_raw_data()[3])
 
 
 if __name__ == "__main__":
@@ -46,7 +53,11 @@ if __name__ == "__main__":
 
     try:
         while True:
-            print("[" + cd.get_red(), cd.get_green(), cd.get_blue() + "]")
+            print("[", cd.get_red(), cd.get_green(), cd.get_blue(), "]")
+
+            
+            if cd.is_yellow:
+                print("yellow")
 
             if cd.is_red():
                 print('red')
